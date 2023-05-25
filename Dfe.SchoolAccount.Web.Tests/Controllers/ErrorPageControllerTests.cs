22b9c03573028f792bc8de3278091724660a3736
Lea Hayes <lea.hayes@education.gov.uk>
2023-05-25 12:56:05 +0100

    Add controller and view for error pages.

---
+++ Dfe.SchoolAccount.Web.Tests/Controllers/ErrorPageControllerTests.cs
@@ -0,0 +1,63 @@
+namespace Dfe.SchoolAccount.Web.Tests.Controllers;
+
+using Dfe.SchoolAccount.Web.Controllers;
+using Dfe.SchoolAccount.Web.Models.Content;
+using Dfe.SchoolAccount.Web.Services.Content;
+using Dfe.SchoolAccount.Web.Tests.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+
+[TestClass]
+public sealed class ErrorPageControllerTests
+{
+    private static ErrorPageController CreateErrorPageControllerWithFakeContent(ErrorPageContent fakeErrorPage)
+    {
+        var logger = new NullLogger<ErrorPageController>();
+
+        var errorPageContentFetcherMock = new Mock<IErrorPageContentFetcher>();
+        errorPageContentFetcherMock.Setup(mock => mock.FetchErrorPageContentAsync(It.Is<string>(slug => slug == "an-example-error-handle")))
+            .ReturnsAsync(fakeErrorPage);
+
+        return new ErrorPageController(logger, errorPageContentFetcherMock.Object);
+    }
+
+    #region Task<IActionResult> Index(string)
+
+    [TestMethod]
+    public async Task Index__ReturnsNotFound__WhenContentForHandleWasNotFound()
+    {
+        var fakeErrorPage = new ErrorPageContent();
+        var errorPageController = CreateErrorPageControllerWithFakeContent(fakeErrorPage);
+
+        var result = await errorPageController.Index("a-slug-that-does-not-exist", 403);
+
+        TypeAssert.IsType<NotFoundObjectResult>(result);
+    }
+
+    [TestMethod]
+    public async Task Index__ReturnsExpectedView()
+    {
+        var fakeErrorPage = new ErrorPageContent();
+        var errorPageController = CreateErrorPageControllerWithFakeContent(fakeErrorPage);
+
+        var result = await errorPageController.Index("an-example-error-handle", 403);
+
+        var viewResult = TypeAssert.IsType<ViewResult>(result);
+        Assert.AreEqual(403, viewResult.StatusCode);
+    }
+
+    [TestMethod]
+    public async Task Index__ProvidesErrorPageContentAsViewModel()
+    {
+        var fakeErrorPage = new ErrorPageContent();
+        var errorPageController = CreateErrorPageControllerWithFakeContent(fakeErrorPage);
+
+        var result = await errorPageController.Index("an-example-error-handle", 403);
+
+        var viewResult = TypeAssert.IsType<ViewResult>(result);
+        Assert.AreSame(fakeErrorPage, viewResult.Model);
+    }
+
+    #endregion
+}
