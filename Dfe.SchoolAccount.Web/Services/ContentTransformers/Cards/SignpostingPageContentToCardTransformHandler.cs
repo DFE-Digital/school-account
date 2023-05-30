4e1914b508a092ca82bd497f64833734df674c71
Lea Hayes <lea.hayes@education.gov.uk>
2023-05-30 09:07:20 +0100

    Update `SignpostingPageContentToCardTransformHandler` to use `IContentHyperlinkResolver` to resolve content hyperlinks.

--- Dfe.SchoolAccount.Web/Services/ContentTransformers/Cards/SignpostingPageContentToCardTransformHandler.cs
+++ Dfe.SchoolAccount.Web/Services/ContentTransformers/Cards/SignpostingPageContentToCardTransformHandler.cs
@@ -1,16 +1,30 @@
 ﻿namespace Dfe.SchoolAccount.Web.Services.ContentTransformers.Cards;
 
 using Dfe.SchoolAccount.Web.Models.Content;
+using Dfe.SchoolAccount.Web.Services.ContentHyperlinks;
 
 public sealed class SignpostingPageContentToCardTransformHandler : IContentModelTransformHandler<SignpostingPageContent, CardModel>
 {
+    private readonly IContentHyperlinkResolver contentHyperlinkResolver;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SignpostingPageContentToCardTransformHandler"/> class.
+    /// </summary>
+    /// <param name="contentHyperlinkResolver">A service to resolve hyperlinks from content references.</param>
+    public SignpostingPageContentToCardTransformHandler(IContentHyperlinkResolver contentHyperlinkResolver)
+    {
+        this.contentHyperlinkResolver = contentHyperlinkResolver;
+    }
+
     /// <inheritdoc/>
     public CardModel TransformContentToModel(SignpostingPageContent content)
     {
+        var contentHyperlink = this.contentHyperlinkResolver.ResolveContentHyperlink(content);
+
         return new CardModel {
             Heading = content.Title,
             Summary = content.Summary,
-            LinkUrl = $"/signposting/{content.Slug}",
+            LinkUrl = contentHyperlink?.Url,
         };
     }
 }
